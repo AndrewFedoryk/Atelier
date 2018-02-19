d359e22abeef59428154a58ca477c3ca44467f48
AndrewFedoryk <36225812+AndrewFedoryk@users.noreply.github.com>
2018-02-19 21:20:20 +0200

    Add js

---
+++ main.js
@@ -0,0 +1,34 @@
+function mathtext() {
+    // alert("ddd");
+    var randomtext=document.MFTF.RandomText.value;
+
+    alert("!!" + randomtext);
+    var reversetext=document.MFTF.ReverseText.value;
+    alert("!!!"+ reversetext);
+
+    if (randomtext==reversetext){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function random() {
+    var text = "";
+    var posible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (var i = 0; i < 5; i++)
+        text += posible.charAt(Math.floor(Math.random() * posible.length))
+    return text;
+}
+    document.getElementById("gen").(function () {
+            document.getElementById("RandomText").value(random())
+        })
+
+
+
+// console.log("!!!"+random())
+
+
+
